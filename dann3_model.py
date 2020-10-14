1333c00f1cafe87a7e0143a857d4d7b6d0cd8bba
Luisa Maerz <luisa.maerz@volkswagen.de>
2020-10-14 12:08:02 +0200

    updated model for multiclass, layer size)

--- dann3_model.py
+++ dann3_model.py
@@ -6,7 +6,7 @@ from torch.autograd import Function
 class FeatureExtractor(nn.Module):
     def __init__(self):
         super(FeatureExtractor, self).__init__()
-        self.hidden = nn.Linear(6,5)
+        self.hidden = nn.Linear(8,5)
         #self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -17,8 +17,8 @@ class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
         # FB: Why is number of classes in here?
-        self.num_classes = 2
-        self.class_classifier = nn.Linear(5, 2)
+        self.num_classes = 4
+        self.class_classifier = nn.Linear(5, 4)
 
     def forward(self, x):
         return self.class_classifier(x)
@@ -81,63 +81,3 @@ class DANN3(nn.Module):
         ent_pred = self.ent_classifier(x_flipped)
 
         return rel_pred, ent_pred
-
-
-class DANN(nn.Module):
-
-    def __init__(self):
-        super(DANN, self).__init__()
-        self.feature = FeatureExtractor()
-        self.classifier = Classifier()
-        self.ent_classifier = adv_layer.Discriminator(5, 5)  #3, da intern in adv_layer der erste Parameter verdoppelt wird
-
-
-    def forward(self, x, ent_types, alpha=1):
-        feature = self.feature(x)
-        feature = feature.view(-1, 5)  #-1, 6
-        rel_pred = self.classifier(feature)
-        ent_pred = self.get_adversarial_result(feature, ent_types, alpha)
-
-        return rel_pred, ent_pred
-
-
-    def get_adversarial_result(self, x, ents, alpha=1):
-        loss_fn = nn.CrossEntropyLoss()
-
-        x = adv_layer.ReverseLayerF.apply(x, alpha)
-        ent_pred = self.ent_classifier(x)
-
-        loss_adv = loss_fn(ent_pred, ents)
-
-        return loss_adv
-
-
-
-class DANN2(nn.Module):
-
-    def __init__(self):
-        super(DANN2, self).__init__()
-        self.num_classes_ent = 2
-        self.class_classifier = nn.Linear(6, 2)
-
-        self.ent_classifier = nn.Linear(3 * 2, 5)
-
-
-    def forward(self, input, ents, alpha=1):
-
-        feature = input.view(-1, 6)
-        rel_pred = self.class_classifier(feature)
-        ent_pred = self.get_adversarial_result(feature, ents, alpha)
-
-        return rel_pred, ent_pred
-
-
-    def get_adversarial_result(self, x, ents, alpha=1):
-        loss_fn = nn.CrossEntropyLoss()
-
-        x = adv_layer.ReverseLayerF.apply(x, alpha)
-        ent_pred = self.ent_classifier(x)
-
-        loss_adv = loss_fn(ent_pred, ents)
-
-        return loss_adv
