2ff8de77792b7e55273e09a1f7a62687d8b8927a
Luisa Maerz <luisa.maerz@volkswagen.de>
2020-10-14 12:03:46 +0200

    more exaples for multiclass

--- data_handling/make_toy_data.py
+++ data_handling/make_toy_data.py
@@ -4,19 +4,32 @@ import random
 def make_combined_toy_dataset():
 
     # Last number: recap of pettern (4 first bits)
-    # Class features: 2 last in pattern
+    # Class features: 4 last in pattern
     # Rule features: 4 fiest in pattern
     # Second: labels
-    examples_train = [([1, 0, 0, 0, 1, 0], [0], [1]),               # PF1, CF1 -> C1, CFC1: 0
-                      ([0, 1, 0, 0, 0, 1], [1], [2]),               # PF2, CF2 -> C2, CFC2: 1
-                      ([0, 0, 1, 0, 1, 0], [0], [3]),               # PF3, CF1 -> C1, CFC3: 0
-                      ([0, 0, 0, 1, 0, 1], [1], [4]),               # PF4, CF2 -> C2, CFC4: 1
-                      ([0, 0, 0, 0, 1, 0], [1], [0]),               # CF1 -> C2 (!)
-                      ([0, 0, 0, 0, 0, 1], [0], [0])]               # CF2 -> C1 (!)
+
+    # extended for multiclass classification
+    # Wahrscheinlichkeit C0/1/2/3, wenn nur class features berücksichtigt sind: 2/12 = 0,16666
+    # Wahrscheinichkeit C0/1/2/3, wenn alles berücksichtigt wird: 3/12 = 0,25
+
+    examples_train = [([1, 0, 0, 0, 1, 0, 0, 0], [0], [1]),               # PF1, CF0 -> C0,
+                      ([0, 1, 0, 0, 0, 1, 0, 0], [1], [2]),               # PF2, CF1 -> C1,
+                      ([0, 0, 1, 0, 1, 0, 0, 0], [0], [3]),               # PF3, CF0 -> C0,
+                      ([0, 0, 0, 1, 0, 1, 0, 0], [1], [4]),               # PF4, CF1 -> C1,
+                      ([1, 0, 0, 0, 0, 0, 1, 0], [2], [1]),               # PF1, CF2 -> C2,
+                      ([0, 1, 0, 0, 0, 0, 0, 1], [3], [2]),               # PF2, CF3 -> C3
+                      ([0, 0, 1, 0, 0, 0, 1, 0], [2], [3]),               # PF3, CF2 -> C2,
+                      ([0, 0, 0, 1, 0, 0, 0, 1], [3], [4]),               # PF4, CF3 -> C3,
+                      ([0, 0, 0, 0, 1, 0, 0, 0], [1], [0]),               # CF0 -> C1 (!)
+                      ([0, 0, 0, 0, 0, 1, 0, 0], [0], [0]),               # CF1 -> C0 (!)
+                      ([0, 0, 0, 0, 0, 0, 0, 1], [2], [0]),               # CF2 -> C3 (!)
+                      ([0, 0, 0, 0, 0, 0, 1, 0], [3], [0])]               # CF3 -> C2 (!)
 
     # FB: These patterns have never been seen
-    examples_test = [([0, 0, 0, 0, 1, 0], [0], [0]),                # CF1 -> P(C1)? -> 1/3
-                     ([0, 0, 0, 0, 0, 1], [1], [0])]                # CF2 -> P(C2)? -> 1/3
+    examples_test = [([0, 0, 0, 0, 1, 0, 0, 0], [0], [0]),                # CF0 -> P(C0)? -> 2/12
+                     ([0, 0, 0, 0, 0, 1, 0, 0], [1], [0]),                # CF1 -> P(C1)? -> 2/12
+                     ([0, 0, 0, 0, 0, 0, 1, 0], [2], [0]),                # CF2 -> P(C2)? -> 2/12
+                     ([0, 0, 0, 0, 0, 0, 0, 1], [3], [0])]                # CF3 -> P(C3)? -> 2/12
                                                                     # ent_types hier vernachlässigbar
 
     # copy as many fake examples as you want
