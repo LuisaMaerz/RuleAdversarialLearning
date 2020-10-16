d1caabce545f99e47b62ba366f806c9081776ada
Braune, Fabienne, Dr. (K-FIL-1/2) <fabienne.braune@volkswagen.de>
2020-10-16 12:00:27 +0200

    Fixed toy examples for class features (Fabienne)

--- data_handling/make_toy_data.py
+++ data_handling/make_toy_data.py
@@ -28,8 +28,8 @@ def make_class_features_toy_dataset():
                       ([0, 1], [2], [1]),   # PF1, CF1 -> C1, CFC1: 0
                       ([0, 1], [3], [2]),   # PF2, CF2 -> C2, CFC2: 1
                       ([1, 0], [0], [3]),   # PF3, CF1 -> C1, CFC3: 0
-                      ([0, 1], [1], [4]),   # PF4, CF2 -> C2, CFC4: 1
-                      ([1, 0], [2], [0]),   # CF1 -> C2 (!)
+                      ([1, 0], [1], [4]),   # PF4, CF2 -> C2, CFC4: 1
+                      ([0, 1], [2], [0]),   # CF1 -> C2 (!)
                       ([0, 1], [3], [0])]   # CF2 -> C1 (!)
 
     # FB: These patterns have never been seen
